97ffe340f427fcdf53d4e08e4889fd743bd7951d
Windson yang <wiwindson@outlook.com>
2018-07-03 23:58:36 +0800

    update style

---
+++ static/js/base.js
@@ -0,0 +1,28 @@
+Vue.component('skills-item', {
+  props: ['skills'],
+  template: `
+    <div class="column skills-div">
+      <p>{{ skills.name }}</p>
+    </div>
+  `
+})
+
+var app = new Vue({ 
+    delimiters: ['[[', ']]'],
+    el: '#coding-title',
+    data: {
+      coding_title: 'Coding is more than algorithms.',
+      seen: true,
+      skills: [
+        { id: 1, thumb: 1, name: 'My journey with Vue' },
+        { id: 2, thumb: 2, name: 'Blogging with Vue' },
+        { id: 3, thumb: 3, name: 'Why Vue is so fun' }
+      ]
+    },
+    methods: {
+      second: function () {
+        this.seen = false
+        this.coding_title = 'Choose your Favourite Topic.'
+    }
+  }
+});
